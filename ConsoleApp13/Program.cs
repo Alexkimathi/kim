da288c0ad510a262456722a1f67a1170940dccdf
AlexKimathi <keemalekiee@gmail.com>
2019-02-22 14:00:20 -0800

    consoleApp13

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] names = {67, 56, 89};
+            
+                string[] name = {"alex", "kim", "joy"};
+                int myvalue = names[1];
+                int mysecondvalue = names[0];
+                int result = myvalue + mysecondvalue;
+            
+            Console.WriteLine(myvalue);
+            Console.WriteLine(mysecondvalue);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        
+    }
+}
